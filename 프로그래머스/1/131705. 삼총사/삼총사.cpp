12115d5470f17f271b39d91ebb94893213d0443e
이승재 <143584323+seungazee@users.noreply.github.com>
2024-03-11 22:24:44 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.cpp
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -8,14 +8,16 @@ int solution(vector<int> number) {
     for(int i = 0; i < number.size() - 2; i++){
         for(int j = i + 1; j < number.size() - 1; j++){
             for(int k = j + 1; k < number.size(); k++){
-                vector<int> sum;
+                /*vector<int> sum;
                 sum.push_back(number[i]);
                 sum.push_back(number[j]);
                 sum.push_back(number[k]);
                 int res = sum[0] + sum[1] + sum[2];
                 if(res == 0)
                     answer += 1;
-                sum.clear();
+                sum.clear();*/
+                if(number[i] + number[j] + number[k] == 0)
+                    answer += 1;
             }
         }
     }
