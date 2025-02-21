5fd73f9fb2f22de9b67e726700bf37e3efdd7bbc
이승재 <143584323+seungzaee@users.noreply.github.com>
2025-02-21 14:17:24 +0900

    [Silver V] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+#include<set>
+#include<map>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int gcd(int a, int b) {
+	while (b != 0) {
+		int temp = a % b;
+		a = b;
+		b = temp;
+	}
+	return a;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long long int num1, num2;
+	cin >> num1 >> num2;
+	int res = gcd(num1, num2);
+	cout << (num1 * num2) / res << "\n";
+
+	return 0;
+}
