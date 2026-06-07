98952ca14b2fad02c3b5abd62f85343bebccb52e
이승재 <143584323+seungzaee@users.noreply.github.com>
2026-06-07 15:35:40 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -0,0 +1,11 @@
+def solution(n):
+    ans = 0
+    
+    while n != 0 :
+        if n % 2 != 0 :
+            n -= 1
+            n /= 2
+            ans += 1
+        else :
+            n /= 2
+    return ans
