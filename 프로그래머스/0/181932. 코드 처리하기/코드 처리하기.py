7b3553ba5c8d129a52daf00ff30beb0108120e55
이승재 <143584323+seungzaee@users.noreply.github.com>
2026-08-03 10:36:26 +0900

    [level 0] Title: 코드 처리하기, Time: 5.96 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,20 @@
+def solution(code):
+    ret = ''
+    mode = 0
+    for i in range (len(code)) :
+        if mode == 0 :
+            if code[i] != '1' :
+                if i % 2 == 0 :
+                    ret += code[i]
+            elif code[i] == '1' :
+                mode = 1
+        else :
+            if code[i] != '1' :
+                if i % 2 == 1 :
+                    ret += code[i]
+            elif code[i] == '1' :
+                mode = 0
+            
+    if len(ret) == 0 :
+        return 'EMPTY'
+    return ret
